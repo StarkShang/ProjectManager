3e09b682f1ee90190bb3461e97a0789b44e6023e
StarkShang <stark.shang@sjtu.edu.cn>
2018-04-21 21:13:46 +0800

    Start up project 'ProjectFinder'

---
+++ ProjectFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
